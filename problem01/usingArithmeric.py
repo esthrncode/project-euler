89169965d3b2670cce69d3d832ff5c1ee59c3276
sthr <162213735+estherNCode@users.noreply.github.com>
2024-03-07 18:07:46 -0800

    Update usingArithmeric.py

--- problem01/usingArithmeric.py
+++ problem01/usingArithmeric.py
@@ -5,5 +5,6 @@ def arithmeticSum(a, maxNumber):
     b = a * n #find the last number in the series
     return n * (a + b) // 2 # apply the arithmetic series sum formula
 
-result = arithmeticSum(3, 1000) + arithmeticSum(5,1000) - arithmeticSum(15,1000)
+result = arithmeticSum(3, 1000) + arithmeticSum(5,1000) - arithmeticSum(15,1000) #avoid double-counting multiples of 15
+
 print(result)
