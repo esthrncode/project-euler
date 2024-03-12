f495d19197c1791376877be8a26ea47d5813da01
sthr <162213735+esthrncode@users.noreply.github.com>
2024-03-11 17:52:23 -0700

    Update solution.py

--- problem04/solution.py
+++ problem04/solution.py
@@ -1,15 +1,13 @@
-# brute force approach
-
 def is_palindrome(n):
-    return str(n) == str(n)[::-1]
+    return str(n) == str(n)[::-1] # converts n to string and compares n to its palindrome
 
 def find_largest_palindrome():
     largest_palindrome = 0
-    for i in range(999, 99, -1):
-        for j in range(i, 99, -1):
+    for i in range(999, 99, -1): # loop from 999 down to 100 with decrement of 1
+        for j in range(i, 99, -1): # loop from i down to 100 with decrement of 1, to avoid redundant checks
             product = i * j
             if product <= largest_palindrome:
-                break
+                break # since we aim to found the largest palindrome and j is decreasing, further products will be smaller, 
             if is_palindrome(product):
                 largest_palindrome = product
                 break
