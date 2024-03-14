cc3fc9de7055c5078bfea0a0f41874a8de14492b
sthr <162213735+esthrncode@users.noreply.github.com>
2024-03-14 16:50:15 -0700

    Update solution.py

--- problem07/solution.py
+++ problem07/solution.py
@@ -1,7 +1,6 @@
 # brute-force approach
-
 def is_prime(n):
-    """Check if a number is prime."""
+    """check if a number is prime."""
     if n <= 1:
         return False
     for i in range(2, int(n**0.5) + 1):
@@ -10,7 +9,7 @@ def is_prime(n):
     return True
 
 def nth_prime(n):
-    count = 0  # Count of prime numbers found
+    count = 0  # xount of prime numbers found
     num = 1 
     while count < n:
         num += 1
@@ -18,7 +17,7 @@ def nth_prime(n):
             count += 1
     return num
 
-# Find the 10001st prime number
+# the 10001st prime number
 print(nth_prime(10001))
 
 #optimal approach
