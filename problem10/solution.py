8f9023f895c40cb4a62a5289cab69c559c29acd4
sthr <162213735+esthrncode@users.noreply.github.com>
2024-03-19 10:01:34 -0700

    Update solution.py

--- problem10/solution.py
+++ problem10/solution.py
@@ -11,9 +11,22 @@ def is_prime(n: int)-> bool:
 
 def sum_primes(limit: int)-> int:
     return sum([i for i in range(2, limit) if is_prime(i)])
+# start = time.time()
+# print(sum_primes(2000000)) '''not recommended'''
+# end = time.time()
+# print("Time:", end - start)
+
+# optimal approach: ussing sieve of eratosthenes
+def sum_primes_optimal(limit: int)-> int:
+    primes = [True] * limit
+    primes[0], primes[1] = False, False
+    for i in range(2, int(math.sqrt(limit)) + 1):
+        if primes[i]:
+            for j in range(i * i, limit, i):
+                primes[j] = False
+    return sum([i for i in range(limit) if primes[i]])
 start = time.time()
-print(sum_primes(2000000))
+print(sum_primes_optimal(2000000))
 end = time.time()
 print("Time:", end - start)
 
-# optimal approach
