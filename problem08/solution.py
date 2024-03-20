b170ff13e0b15aa66e7bc29ba81b0edb6ea39b3d
sthr <162213735+esthrncode@users.noreply.github.com>
2024-03-20 11:39:07 -0700

    Create solution.py

---
+++ problem08/solution.py
@@ -0,0 +1,26 @@
+# brute-force approach
+
+from time import perf_counter
+
+def find_greatest_product(number, k):
+    digit_list = [int(digit) for digit in str(number)]
+    
+    max_product = 0
+    
+    for i in range(len(digit_list) - k + 1):
+        product = 1
+        for j in range(k):
+            product *= digit_list[i + j]
+        
+        max_product = max(max_product, product)
+    
+    return max_product
+
+number_str = "73167176531330624919225119674426574742355349194934"  # Add the rest of the number given in the problem
+k = 4  
+
+start_time = perf_counter()
+print(find_greatest_product(number_str, k))
+end_time = perf_counter()
+
+print( end_time - start_time)
