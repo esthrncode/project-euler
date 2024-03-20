80e49bd4153b153f36685928459eafabee2841d0
sthr <162213735+esthrncode@users.noreply.github.com>
2024-03-20 12:03:46 -0700

    Update solution.py

--- problem08/solution.py
+++ problem08/solution.py
@@ -1,47 +1,43 @@
-# brute-force approach
-
 from time import perf_counter
 
-def find_greatest_product(number_str, k: int)-> int:
-    digit_list = [int(digit) for digit in str(number_str)]
+# brute-force approach
+
+def find_greatest_product(number_str, k: int) -> int:
+    clean_str = number_str.replace(" ", "") 
+    clean_str = clean_str.replace("\n", "")
     max_product = 0
-    for i in range(len(digit_list) - k + 1):
+        
+    for i in range(len(clean_str) - k + 1):
         product = 1
         for j in range(k):
-            product *= digit_list[i + j]
+            product *= int(clean_str[i + j])
+        
         max_product = max(max_product, product)
+    
     return max_product
-
-# optimal approach
-
-def find_greatest_product_optimal(number_str, k: int)-> int:
-    digit_list = [int(digit) for digit in str(number_str)]
-    max_product = 0
-    zero_index = -1
-    for i in range(len(digit_list) - k + 1):
-        if i <= zero_index:
-            continue
-        product = 1
-        for j in range(i, i + k):
-            if digit_list[j] == 0:
-                zero_index = j
-                break
-            product *= digit_list[j]
-        else:
-            max_product = max(max_product, product)
-    return max_product
-
-number_str = "73167176531330624919225119674426574742355349194934" 
-k = 13 
+k = 13
+number_str = """73167176531330624919225119674426574742355349194934
+            96983520312774506326239578318016984801869478851843
+            85861560789112949495459501737958331952853208805511
+            12540698747158523863050715693290963295227443043557
+            66896648950445244523161731856403098711121722383113
+            62229893423380308135336276614282806444486645238749
+            30358907296290491560440772390713810515859307960866
+            70172427121883998797908792274921901699720888093776
+            65727333001053367881220235421809751254540594752243
+            52584907711670556013604839586446706324415722155397
+            53697817977846174064955149290862569321978468622482
+            83972241375657056057490261407972968652414535100474
+            82166370484403199890008895243450658541227588666881
+            16427171479924442928230863465674813919123162824586
+            17866458359124566529476545682848912883142607690042
+            24219022671055626321111109370544217506941658960408
+            07198403850962455444362981230987879927244284909188
+            84580156166097919133875499200524063689912560717606
+            05886116467109405077541002256983155200055935729725
+            71636269561882670428252483600823257530420752963450"""
 
 start_time = perf_counter()
-result_brute_force = find_greatest_product(number_str, k)
+print(find_greatest_product(number_str, k))
 end_time = perf_counter()
-print(result_brute_force)
-print(end_time - start_time)
-
-start_time = perf_counter()
-result_optimal = find_greatest_product_optimal(number_str, k)
-end_time = perf_counter()
-print(result_optimal)
 print(end_time - start_time)
